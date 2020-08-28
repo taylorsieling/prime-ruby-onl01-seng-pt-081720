38819f67b02dbd90aee21c8974e39eeffc58b767
Taylor Sieling <taylorasieling@gmail.com>
2020-08-28 01:48:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(x)
+    if x <= 1
+      return false
+    elsif (2...x).any? {|n| x % n == 0}
+            return false
+        else
+            return true
+    end
+end
